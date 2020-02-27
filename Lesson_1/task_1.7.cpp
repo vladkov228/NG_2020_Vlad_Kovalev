ecdffc58823f4b5ac593a022034c68c4d31d6915
vladkov228 <vladsterkov@gmail.com>
2020-02-27 19:22:15 +0200

    add task_1.7 first lesson

--- Lesson_1/task_1.7.cpp
+++ Lesson_1/task_1.7.cpp
@@ -7,5 +7,20 @@ int main()
     int zarplata;
     cout << "vvedite zarplatu: ";
     cin >> zarplata;
-    if (zarplata > 1000)
+    if (zarplata > 999)
+    {
+         if (zarplata < 1000000)
+        {
+            cout << "Horosho,";
+        }
+    }
+    if (zarplata < 1000)
+    {
+        cout << "Rabotai bolshe,";
+    }
+    if (zarplata > 999999)
+        {
+        cout << "Ti milioner,";
+        }
+    cout << "no ti molodec";
 }
