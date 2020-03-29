94968c04595420920de29473b8ccaf0c276162c3
vladkov228 <vladsterkov@gmail.com>
2020-03-29 19:26:25 +0300

    Fixed lesson 4 task 1

--- Lesson_4/task_4.1.cpp
+++ Lesson_4/task_4.1.cpp
@@ -4,26 +4,28 @@ using namespace std;
 
 int main()
 {
-  char stroka[1000];
-  int a=0;
-  int symvol=0;
-  bool probel = true;
+    char stroka[1000];
+    int counter = 0;
+    int symvol = 0;
+    bool endWord = true;
 
-  cout << "Enter your string: ";
-  cin.getline(stroka,1000);
+    cout << "Enter your string: ";
+    cin.getline(stroka, 1000);
 
-  while(stroka[a] != NULL)
-  {
-     if(stroka[a] == ' ')
-         probel= true;
-     else if ( probel )
-     {
-        symvol ++;
-        probel = false;
-     }
-     a++;
-  }
-  cout << "\nWords in a sentence: " << symvol << endl;
+    while (stroka[counter] != 0)
+    {
+        if (!(stroka[counter] >= 'a') and (stroka[counter] <= 'z') or (stroka[counter] >= 'A') and (stroka[counter] <= 'Z'))
+        {
+            endWord = true;
+        }
+        else if (endWord)
+        {
+            symvol++;
+            endWord = false;
+        }
+        counter++;
+    }
+    cout << "\nWords in a sentence: " << symvol << endl;
 }
 
 
