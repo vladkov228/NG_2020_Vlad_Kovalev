e1543814470ac5589a78c50697fa7b3b3b4648b2
vladkov228 <vladsterkov@gmail.com>
2020-04-17 13:54:07 +0300

    Fixed identation in lesson 2 task 4

--- Lesson_2/task_2.4.cpp
+++ Lesson_2/task_2.4.cpp
@@ -4,26 +4,26 @@ using namespace std;
 
 int main()
 {
-  int stairs = 0, index=0, counter=1, counterOutput=0;
+      int stairs = 0, index=0, counter=1, counterOutput=0;
 
-  cout << "Enter the amount of stairs/backstairs/pyramid: ";
-  cin >> stairs;
+      cout << "Enter the amount of stairs/backstairs/pyramid: ";
+      cin >> stairs;
 
-      while (index<stairs)
-      {
-          while(counterOutput < counter )
+          while (index<stairs)
           {
-              cout << "*";
-              counterOutput++;
+              while(counterOutput < counter )
+              {
+                  cout << "*";
+                  counterOutput++;
+              }
+              counterOutput =0;
+              counter++;
+              index++;
+          cout << endl;
           }
-      counterOutput =0;
-      counter++;
-      index++;
       cout << endl;
-      }
-  cout << endl;
-  index=0;
-  counter = stairs;
+      index=0;
+      counter = stairs;
 
       while (index < stairs )
       {
@@ -39,25 +39,25 @@ int main()
       }
       cout << endl;
 
-  index = 0;
-  counter = 0;
-  counterOutput = 0;
-       for (int index = 0; index < stairs; index++)
-       {
+      index = 0;
+      counter = 0;
+      counterOutput = 0;
+      for (int index = 0; index < stairs; index++)
+      {
 
-            for (int counterOutput = 1; counterOutput < stairs - index; counterOutput++)
-            {
-                cout << ' ';
-            }
+           for (int counterOutput = 1; counterOutput < stairs - index; counterOutput++)
+           {
+               cout << ' ';
+           }
 
-            for (int counterOutput = stairs - 2 * index; counterOutput <= stairs; counterOutput++)
-            {
-                cout << '*';
-            }
+           for (int counterOutput = stairs - 2 * index; counterOutput <= stairs; counterOutput++)
+           {
+               cout << '*';
+           }
 
-            cout << endl;
-       }
+           cout << endl;
+      }
 
 
-        cout << endl;
+      cout << endl;
 }
