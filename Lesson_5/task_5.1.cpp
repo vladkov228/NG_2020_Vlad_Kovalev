ededdda2e2463124bf69f42b3f1bee5037490eec
vladkov228 <vladsterkov@gmail.com>
2020-04-22 13:02:05 +0300

    Fixed function

--- Lesson_5/task_5.1.cpp
+++ Lesson_5/task_5.1.cpp
@@ -1,28 +1,28 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
 
-int func(int arr[], int numb)
+void func(int arr[],int siz, int num)
 {
-   for(int index=0; index < 5; index++)
-   {
-       cout <<  arr[index]* numb << endl;
-
-   }
+    for (int index=0; index < siz; index++)
+    {
+        cout << arr[index] * num << endl;
+    }
 }
-
 
 int main()
 {
-   int arr[5], numb;
-   cout << "Enter array : " << endl;
-   for(int index=0; index < 5; index++)
-   {
-       cout << index + 1 << ". element: ";
-       cin >> arr[index];
-   }
-   cout << "Enter a number : ";
-   cin >> numb;
-   func( arr, numb);
-}
+    int siz_arr, index, mult;
 
+    cout << "Enter size array: ";
+    cin >> siz_arr;
+    int funct[siz_arr];
+    for (index = 0; index < siz_arr; index++)
+    {
+        cout << "Enter number: ";
+        cin >> funct[index];
+    }
+    cout << endl << "Enter number for multiply : " ;
+    cin >> mult;
+
+    func(funct, siz_arr, mult );
+}
