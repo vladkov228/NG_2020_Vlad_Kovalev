378dafa9df62b1c348db5948003627e7edaee3ff
vladkov228 <vladsterkov@gmail.com>
2020-04-27 17:48:03 +0300

    Add task 4.3

---
+++ Lesson_4/task_4.3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char string[40], letters = 0;
+    cout << "Enter your string: ";
+    cin.getline(string,100);
+    while(string[letters] != NULL)
+    {
+        if(string[letters] >= 'a' and string[letters] <= 'z')
+        {
+            cout << char(string[letters] - 32);
+        }
+        else
+        {
+            cout << string[letters];
+        }
+        letters++;
+    }
+    cout << endl;
+}
