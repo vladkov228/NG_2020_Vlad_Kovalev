7eeb6f6c480f17a54f69d5e40eb924b3960076d2
vladkov228 <vladsterkov@gmail.com>
2020-05-16 00:05:02 +0300

    Fixed task 5.3

--- Lesson_5/task_5.1.cpp
+++ Lesson_5/task_5.1.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 
-void func(int arr[],int siz, int num)
+void func(int arr[], int siz, int num)
 {
-    for (int index=0; index < siz; index++)
+    for (int index = 0; index < siz; index++)
     {
         arr[index] *= num;
-        cout << arr[index] << endl;
     }
+
 }
 
 int main()
@@ -16,14 +16,23 @@ int main()
 
     cout << "Enter size array: ";
     cin >> siz_arr;
+    if (siz_arr < 0)
+    {
+        cout << "Error";
+        return 0;
+    }
     int funct[siz_arr];
     for (index = 0; index < siz_arr; index++)
     {
         cout << "Enter number: ";
         cin >> funct[index];
     }
-    cout << endl << "Enter number for multiply : " ;
+    cout << endl << "Enter number for multiply : ";
     cin >> mult;
 
-    func(funct, siz_arr, mult );
+    func(funct, siz_arr, mult);
+    for (int index = 0; index < siz_arr; index++)
+    {
+        cout << " => " << funct[index] << endl;
+    }
 }
